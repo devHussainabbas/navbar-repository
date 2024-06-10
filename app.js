07c161f5bd3cbf67b6d564bed0ac3240ce426aba
devHussainabbas <03555abc@gmail.com>
2024-06-10 21:25:12 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,96 @@
+function hussain(){
+    document.getElementById("main").style.backgroundColor="aqua";
+    document.getElementById("main").style.color="black";
+    document.getElementById("main").style.borderColor="black";
+
+}
+function abbas(){
+    document.getElementById("main").style.backgroundColor="red";
+    document.getElementById("main").style.color="white";
+    document.getElementById("main").style.borderColor="blue";
+
+}
+function arbab(){
+    document.getElementById("main").style.backgroundColor="green";
+    document.getElementById("main").style.color="blue";
+    document.getElementById("main").style.borderColor="red";
+
+}
+function hisper(){
+    document.getElementById("main").style.backgroundColor="orangered";
+    document.getElementById("main").style.color="black";
+    document.getElementById("main").style.borderColor="purple";
+
+}
+function hoper(){
+    document.getElementById("main").style.backgroundColor="purple";
+    document.getElementById("main").style.color="white";
+    document.getElementById("main").style.borderColor="yellow";
+}
+
+
+// ever tach stnd[]
+
+
+// =======================recall
+let student=["jamshad","kashif","hussain",14,12,5];
+
+// console.log(student.length);
+// console.log(student[2]);
+
+
+// let cities={
+// fname:"hussain",
+// sname:"jamshad",
+// tname:"kashif",
+// nmbr:3,
+// };
+
+
+
+// console.log(cities["tname"]);
+// let a="    jamal      ";
+// console.log(a);
+// console.log(a.trim());
+// console.log(a.trimStart());
+// console.log(a.trimEnd());
+
+// let t=14.5;
+// console.log(Math.ceil(t));
+
+
+
+
+
+
+
+// console.log(a.charAt([4]));
+// let c=a.toUpperCase()
+// console.log(c);
+// let b="JAMAL";
+// let f=b.toLowerCase()
+// console.log(f);
+
+
+
+// console.log(a.length);
+
+// let t="Asif,Ustad,ran";
+// console.log(t);
+// console.log(t.split());
+
+
+
+
+// let text = "hussain";
+// document.write(text.toUpperCase())
+
+// let g="arbab";
+// document.write(g.charAt(1))
+
+let text= [8,9,10]
+text.unshift(1,2,3,4,5,6,7)
+console.log(text) ;
+
+
+
